d7bca9e6722770ea8d8c0d8509b878faa7f142ee
sergripenko <sergripenko@gmail.com>
2020-05-12 19:29:54 +0300

    create Images table

---
+++ db/migrations/20200512191556_AddImagesTable.sql
@@ -0,0 +1,20 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE images (
+    id          SERIAL PRIMARY KEY,
+    user_id     INT NOT NULL,
+    title       VARCHAR(50),
+    height      INT,
+    width       INT,
+    uniq_id     VARCHAR(50),
+    url         VARCHAR(255),
+    created_at  TIMESTAMP,
+    updated_at  TIMESTAMP,
+    deleted_at  TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE IF EXISTS images;
