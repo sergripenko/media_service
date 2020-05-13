473d775efc12f41501b0b8a3a40f52e1757c4c2a
sergripenko <sergripenko@gmail.com>
2020-05-13 15:15:21 +0300

    add BaseController

---
+++ controllers/default.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/astaxie/beego"
+
+type BaseController struct {
+	beego.Controller
+}
+
+func (c *BaseController) Response(status int, data interface{}, err error) {
+
+	if err != nil {
+		c.Data["json"] = err.Error()
+
+	} else {
+		c.Data["json"] = data
+	}
+	c.Ctx.Output.SetStatus(status)
+	c.ServeJSON()
+}
