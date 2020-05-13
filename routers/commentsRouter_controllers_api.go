a096dc3fb7116f2a0642287df2f5913e7c3e93bf
sergripenko <sergripenko@gmail.com>
2020-05-13 15:15:34 +0300

    autogenerated

---
+++ routers/commentsRouter_controllers_api.go
@@ -0,0 +1,100 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:ImagesController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:UsersController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:UsersController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:UsersController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:UsersController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:UsersController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:UsersController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:UsersController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:UsersController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media_service/controllers/api:UsersController"] = append(beego.GlobalControllerRouter["media_service/controllers/api:UsersController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+}
