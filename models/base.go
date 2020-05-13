d1f133beec5fffd3104919592468de7679f7330d
sergripenko <sergripenko@gmail.com>
2020-05-13 15:15:50 +0300

    update times fields

--- models/base.go
+++ models/base.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Base struct {
-	CreatedAt time.Time `orm:"column(created_at);auto_now_add;type(timestamp without time zone);null"`
-	UpdatedAt time.Time `orm:"column(updated_at);auto_now;type(timestamp without time zone);null"`
-	DeletedAt time.Time `orm:"column(deleted_at);type(timestamp without time zone);null"`
+	CreatedAt time.Time `orm:"column(created_at);auto_now_add;type(timestamp without time zone);null" json:"created_at"`
+	UpdatedAt time.Time `orm:"column(updated_at);auto_now;type(timestamp without time zone);null" json:"updated_at"`
+	DeletedAt time.Time `orm:"column(deleted_at);type(timestamp without time zone);null" json:"deleted_at"`
 }
